e10e0dd24945da4a66ae56d513a3c92f8d610b2f
Marc <RhinoDevel@users.noreply.github.com>
2020-03-22 05:35:03 +0100

    Improve comment.

--- but.asm
+++ but.asm
@@ -17,8 +17,8 @@
 ; ***
 ; *** output:
 ; *** -------
-; *** a = 0, if pressed. not 0, if not pressed.
-; *** z-flag = 0, if pressed. 1, if not pressed.
+; *** a = 0, if pressed(!). not 0, if not pressed.
+; *** z-flag = 0, if pressed(!). 1, if not pressed.
 ; *** x = screen (not petscii) code of button to check (kept).
 ; ***
 ;
