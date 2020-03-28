677457fae227178a33aab02067576b132c565a04
Marc <RhinoDevel@users.noreply.github.com>
2020-03-28 22:53:45 +0100

    Improve goodbye text.

--- goodbye.asm
+++ goodbye.asm
@@ -5,26 +5,37 @@
 ; --- message to show after exiting (screen codes) ---
 ; ----------------------------------------------------
 
-goodbye$ byte 5, 3, 34
+goodbye$ byte 4, 9, 21
+         byte 146, 136, 137, 142, 143, 132, 133, 150, 133, 140
+         byte 167 ; '
+         byte 147, 160, 139, 133, 153, 130, 143, 129, 146, 132
+
+         byte 6, 3, 34
          text '**********************************'
 
-         byte 6, 2, 36
+         byte 7, 2, 36
          text '*** goodbye and have a nice day! ***'
 
-         byte 7, 3, 34
+         byte 8, 3, 34
          text '**********************************'
 
-         byte 9, 2, 36
+         byte 10, 0, 39
+         text '++ (c) 2020, rhinodevel, marcel timm ++'
+
+         byte 12, 0, 40
+         text '-- source code: github.com/rhinodevel --'
+
+         byte 14, 2, 36
          text '--- contact: '
          byte 'm', 'a', 'r', 'c', '@'
          byte 'r', 'h', 'i', 'n', 'o', 'd', 'e', 'v', 'e', 'l', '.'
          byte 'c', 'o', 'm'
          text ' ---'
 
-         byte 11, 2, 36
+         byte 16, 2, 36
          text '--- website: www.rhinodevel.com  ---'
 
-         byte 13, 1, 38
-         text '>>> type run to return to keyboard <<<'
+         byte 18, 0, 39
+         text '>>> type run to return to keyboard. <<<'
 
          byte $ff ; end marker.
