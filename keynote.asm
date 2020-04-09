a002e434b1df7d2f955c72131eea33c5de09785f
Marc <RhinoDevel@users.noreply.github.com>
2020-04-09 21:55:19 +0200

    Cosmetic.

--- keynote.asm
+++ keynote.asm
@@ -10,31 +10,31 @@
 ;
 keynote$ byte $00 ; @
          byte $00 ; a
-         byte 166 ; b * 1. G
-         byte 198 ; c * 1. E
-         byte 210 ; d * 1. D#
-         byte  98 ; e * 2. E
+         byte 166 ; b * 1. g
+         byte 198 ; c * 1. e
+         byte 210 ; d * 1. d#
+         byte  98 ; e * 2. e
          byte $00 ; f
-         byte 176 ; g * 1. F# 
-         byte 157 ; h * 1. G#
-         byte  61 ; i * 3. C (highest note) 
-         byte 139 ; j * 1. A#
+         byte 176 ; g * 1. f# 
+         byte 157 ; h * 1. g#
+         byte  61 ; i * 3. c (highest note) 
+         byte 139 ; j * 1. a#
          byte $00 ; k
          byte $00 ; l
-         byte 132 ; m * 1. B
-         byte 148 ; n * 1. A
+         byte 132 ; m * 1. b
+         byte 148 ; n * 1. a
          byte $00 ; o
          byte $00 ; p
-         byte 124 ; q * 2. C (one of two buttons)
-         byte  92 ; r * 2. F
-         byte 236 ; s * 1. C#
-         byte  82 ; t * 2. G
-         byte  65 ; u * 2. B
-         byte 187 ; v * 1. F 
-         byte 110 ; w * 2. D
-         byte 223 ; x * 1. D
-         byte  73 ; y * 2. A
-         byte 250 ; z * 1. C (lowest note)
+         byte 124 ; q * 2. c (one of two buttons)
+         byte  92 ; r * 2. f
+         byte 236 ; s * 1. c#
+         byte  82 ; t * 2. g
+         byte  65 ; u * 2. b
+         byte 187 ; v * 1. f 
+         byte 110 ; w * 2. d
+         byte 223 ; x * 1. d
+         byte  73 ; y * 2. a
+         byte 250 ; z * 1. c (lowest note)
          byte $00 ; [
          byte $00 ; \
          byte $00 ; ]
@@ -42,17 +42,17 @@ keynote$ byte $00 ; @
          byte $00 ; <left arrow>
          byte $00 ; <space>
          byte $00 ; !
-         byte 117 ; <quotation mark> * 2. C#
-         byte 104 ; # * 2. D#
+         byte 117 ; <quotation mark> * 2. c#
+         byte 104 ; # * 2. d#
          byte $00 ; $
-         byte  87 ; % * 2. F#
-         byte  69 ; & * 2. A#
-         byte  77 ; ' * 2. G#
+         byte  87 ; % * 2. f#
+         byte  69 ; & * 2. a#
+         byte  77 ; ' * 2. g#
          byte $00 ; (
          byte $00 ; )
          byte $00 ; *
          byte $00 ; +
-         byte 124 ; , * 2. C (one of two buttons)
+         byte 124 ; , * 2. c (one of two buttons)
          byte $00 ; -
          byte $00 ; .
          byte $00 ; /
