0ad6ae8526888978fbd3f5e2c975003d08d6bdba
Marc <RhinoDevel@users.noreply.github.com>
2020-04-12 22:32:05 +0200

    Add (unused) different patterns.

---
+++ patterns.asm
@@ -0,0 +1,29 @@
+
+; marcel timm, rhinodevel, 2020apr12
+
+; ----------------------------------------------------------------
+; --- all shift patterns that are different for the human ear. ---
+; ---                                                          ---
+; --- some patterns will alter the octave.                     ---
+; ----------------------------------------------------------------
+
+pattern_count$ = 17
+
+patterns$
+         byte %00000001
+         byte %00000011
+         byte %00000101
+         byte %00000111
+         byte %00001001
+         byte %00001011
+         byte %00001111
+         byte %00010001
+         byte %00010011
+         byte %00010101
+         byte %00010111
+         byte %00011011
+         byte %00100101
+         byte %00101011
+         byte %00101101
+         byte %00110011
+         byte %01010101
