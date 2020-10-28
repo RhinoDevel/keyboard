b70d0f71a24f0b1fa71f7a87bdcc4e110a817328
Marc <RhinoDevel@users.noreply.github.com>
2020-10-28 21:35:58 +0100

    Improve file mask further.

--- filemask.asm
+++ filemask.asm
@@ -6,49 +6,49 @@
 ; ------------------------------------------------------------------
 
 filemask$
-         byte 0
-         byte 0
+         byte 1
+         byte 1
          byte 21
          byte 177
          text ' = load from tape #1'
 
-         byte 2
-         byte 0
+         byte 3
+         byte 1
          byte 21
          byte 178
          text ' = load from tape #2'
 
-         byte 5
-         byte 0
+         byte 6
+         byte 1
          byte 19 
          byte 182
          text ' = save to tape #1'
 
-         byte 7
-         byte 0
+         byte 8
+         byte 1
          byte 19 
          byte 183
          text ' = save to tape #2'
 
-         byte 10
-         byte 0
+         byte 11
+         byte 1
          byte 10
          byte 131
          text ' = cancel'
 
-         byte 13
-         byte 0
+         byte 14
+         byte 1
          byte 6
          text 'hints:'
 
-         byte 15
-         byte 0
-         byte 39
-         text '- forward or rewind tape now, if needed'
+         byte 16
+         byte 1
+         byte 38
+         text '- forward / rewind tape now, if needed'
 
-         byte 17
-         byte 0
-         byte 40
-         text '- if tape fails, go to basic & enter run'
+         byte 18
+         byte 1
+         byte 37
+         text '- on failure, go to basic & enter run'
 
          byte $ff ; end marker.
