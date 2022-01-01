d1861b3fbec14737bfdbb5e8a4a9147bfaee7646
Marc <RhinoDevel@users.noreply.github.com>
2022-01-01 13:55:42 +0100

    BUGFIX: Use correct (and partially improved) note values from C"1" to C"3", no longer from B"1" to B"3".

--- notes.asm
+++ notes.asm
@@ -5,30 +5,30 @@
 ; --- musical notes' values (to fill via timer 2 's low byte with) ---
 ; --------------------------------------------------------------------
 
-notes$   byte 250 ; 1. c
-         byte 236 ; 1. c#
-         byte 223 ; 1. d
-         byte 210 ; 1. d#
-         byte 198 ; 1. e
-         byte 187 ; 1. f
-         byte 176 ; 1. f#
-         byte 166 ; 1. g
-         byte 157 ; 1. g#
-         byte 148 ; 1. a
-         byte 139 ; 1. a#
-         byte 132 ; 1. b
+notes$   byte 236 ; 1. c
+         byte 223 ; 1. c#
+         byte 210 ; 1. d
+         byte 198 ; 1. d#
+         byte 187 ; 1. e
+         byte 176 ; 1. f
+         byte 167 ; 1. f#
+         byte 157 ; 1. g
+         byte 148 ; 1. g#
+         byte 140 ; 1. a
+         byte 132 ; 1. a#
+         byte 125 ; 1. b
 
-         byte 124 ; 2. c ; index 12
-         byte 117 ; 2. c#
-         byte 110 ; 2. d
-         byte 104 ; 2. d#
-         byte  98 ; 2. e
-         byte  92 ; 2. f
-         byte  87 ; 2. f#
-         byte  82 ; 2. g
-         byte  77 ; 2. g#
-         byte  73 ; 2. a
-         byte  69 ; 2. a#
-         byte  65 ; 2. b
+         byte 117 ; 2. c ; index 12
+         byte 111 ; 2. c#
+         byte 104 ; 2. d
+         byte  98 ; 2. d#
+         byte  93 ; 2. e
+         byte  87 ; 2. f
+         byte  82 ; 2. f#
+         byte  77 ; 2. g
+         byte  73 ; 2. g#
+         byte  69 ; 2. a
+         byte  65 ; 2. a#
+         byte  61 ; 2. b
 
-         byte  61 ; 3. c
+         byte  58 ; 3. c
